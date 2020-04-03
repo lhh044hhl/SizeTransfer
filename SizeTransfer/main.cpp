fe42831f69457504c0c40c9f8b9dc8a6075a9e77
113295634@qq.com <113295634@qq.com>
2020-04-03 15:49:00 +0800

    练习1-5

--- SizeTransfer/main.cpp
+++ SizeTransfer/main.cpp
@@ -64,7 +64,7 @@ Point2f(200,100)
 }*/
 
 //练习四
-int main() {
+/*int main() {
 	Mat dstMat;
 	Mat srcMat = imread("E:\\dignal picture\\lena.jpg", 1);
 	if (srcMat.empty()) return -1;
@@ -84,6 +84,47 @@ Point2f(340,180)
 	warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
 	imshow("src", srcMat);
 	imshow("dst", dstMat);
+	waitKey(0);
+
+
+}*/
+
+//练习五
+int main() {
+	int n=0;
+	Mat dstMat;
+	Mat srcMat = imread("E:\\dignal picture\\lena_rot.jpg", 1);
+	float height = srcMat.rows;
+	float width = srcMat.cols;
+
+	if (srcMat.empty()) return -1;
+
+	for (float i = 1;i < width;i++) {
+		if (srcMat.at<Vec3b>(1, i-1)[0] - srcMat.at<Vec3b>(1, i)[0] > 10 & srcMat.at<Vec3b>(1, i - 1)[1] - srcMat.at<Vec3b>(1, i)[1] > 10 & srcMat.at<Vec3b>(1, i - 1)[2] - srcMat.at<Vec3b>(1, i)[2] > 10)
+		{
+			n = i;
+			break;
+		}
+	}
+	float c1 = sqrt((width / 2 - n)*(width / 2 - n) + (height*height) / 4);
+	float c2 = sqrt(width*width/4+height*height/4);
+	float scale = c2 /c1;//求尺寸
+	//求角度
+	float cosa = (c1*c1 + c2 * c2 - ( n)*(n)) / (2 * c1*c2);
+	float angle_1 = acos(cosa);
+	float angle = angle_1 * 180 / 3.1415;
+	cout << n << endl;
+	cout << width << endl;
+	cout << c1 << endl;
+	cout << c2 << endl;
+	cout << cosa << endl;
+	cout << angle_1 << endl;
+	Point2f center(srcMat.cols*0.5, srcMat.rows*0.5);
+	Mat affine_matrix = getRotationMatrix2D(center, angle, scale);
+	warpAffine(srcMat, dstMat, affine_matrix, srcMat.size());
+	imshow("src", srcMat);
+	imshow("dst", dstMat);
+	
 	waitKey(0);
 
 
